417684c399c3581892b928c0d7171c2ceb9a3148
MrBacanudo <73220344+MrBacanudo@users.noreply.github.com>
2023-01-21 20:56:41 +0100

    Start with any Quintessence

---
+++ StartingQuintessence/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace QuintessenceStart;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(QuintessencePatch));
+        Logger.LogInfo($"Mod {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
---
+++ StartingQuintessence/QuintessencePatch.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Characters.Gear;
+using GameResources;
+using Hardmode.Darktech;
+using HarmonyLib;
+using Level;
+using Services;
+using Singletons;
+using UnityEngine;
+
+namespace QuintessenceStart;
+
+[HarmonyPatch(typeof(LevelManager), "InvokeOnMapChanged")]
+public class QuintessencePatch
+{
+    static void Prefix(ref LevelManager __instance)
+    {
+        ref var self = ref __instance;
+
+        // We don't care about rooms that are not the Dark Castle
+        if (self.currentChapter.type != Chapter.Type.HardmodeCastle)
+        {
+            return;
+        }
+
+        //Singleton<Scenes.Scene>.
+
+        // Try to find a quintessence machine. If there's none, we need to create one.
+        var quintMachine = GameObject.Find("QuintessenceManufacturingMachine");
+
+        if (quintMachine == null)
+        {
+            var itemMachine = GameObject.Find("ItemManufacturingMachine");
+
+            if(itemMachine == null){
+                Debug.LogError("StartingQuintessence mod: Could not find an Item Machine to clone.");
+                return;
+            }
+
+            quintMachine = (GameObject)GameObject.Instantiate(itemMachine, itemMachine.transform.parent);
+            quintMachine.name = "QuintessenceManufacturingMachine";
+            quintMachine.transform.position = itemMachine.transform.position - new Vector3(3.45f, 0, 0);
+
+            var machine = quintMachine.GetComponentInChildren<ManufacturingMachineInteractive>();
+            machine._type = Gear.Type.Quintessence;
+            machine._selectCount = 1;
+            machine._remainSelectCount = 1;
+        }
+
+    }
+}
