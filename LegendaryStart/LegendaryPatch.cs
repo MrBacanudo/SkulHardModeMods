60d963c35e02e5fd60f2b004eedd462dcb5212f1
MrBacanudo <73220344+MrBacanudo@users.noreply.github.com>
2023-01-21 19:09:52 +0100

    Publicize everything!

--- LegendaryStart/LegendaryPatch.cs
+++ LegendaryStart/LegendaryPatch.cs
@@ -10,18 +10,17 @@ using Singletons;
 [HarmonyPatch(typeof(ManufacturingMachineInteractive), "ActivateMachine")]
 public class LegendaryPatch
 {
-    static void Prefix(ref ManufacturingMachineInteractive __instance, ref List<GearReference> ____gearList, ref Gear.Type ____type){
-        // Private property with get accessor, so we're using reflection instead of the triple underscore injection (?)
-        PropertyInfo checkEnhanced = typeof(ManufacturingMachineInteractive).GetProperty("isEnhanced",  BindingFlags.NonPublic | BindingFlags.Instance);
-        bool isEnhanced = (bool)checkEnhanced.GetValue(__instance);
+    static void Prefix(ref ManufacturingMachineInteractive __instance){
+        // Pretty!
+        ref var self = ref __instance;
 
         // We already have added the rares if we're in Level 8+, so we skip to prevent duplication here
-        if (!(isEnhanced && ____type == Gear.Type.Weapon)){
-            ____gearList.AddRange(Singleton<Service>.Instance.gearManager.GetGearListByRarity(____type, Rarity.Rare));
+        if (!(self.isEnhanced && self._type == Gear.Type.Weapon)){
+            self._gearList.AddRange(Singleton<Service>.Instance.gearManager.GetGearListByRarity(self._type, Rarity.Rare));
         }
 
         // Uniques and Legendaries
-        ____gearList.AddRange(Singleton<Service>.Instance.gearManager.GetGearListByRarity(____type, Rarity.Unique));
-        ____gearList.AddRange(Singleton<Service>.Instance.gearManager.GetGearListByRarity(____type, Rarity.Legendary));
+        self._gearList.AddRange(Singleton<Service>.Instance.gearManager.GetGearListByRarity(self._type, Rarity.Unique));
+        self._gearList.AddRange(Singleton<Service>.Instance.gearManager.GetGearListByRarity(self._type, Rarity.Legendary));
     }
 }
