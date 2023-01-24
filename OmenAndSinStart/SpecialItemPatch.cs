04e866a6c7a96d9d156803cdfdc57d9eb5e84b6c
MrBacanudo <73220344+MrBacanudo@users.noreply.github.com>
2023-01-24 23:24:14 +0100

    Allow Starting with Special Items

---
+++ OmenAndSinStart/SpecialItemPatch.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Reflection;
+using Characters.Gear;
+using GameResources;
+using Hardmode.Darktech;
+using HarmonyLib;
+using Services;
+using Singletons;
+using System.Linq;
+using Characters.Gear.Synergy.Inscriptions;
+
+namespace OmenAndSinStart;
+
+[HarmonyPatch(typeof(ManufacturingMachineInteractive), "ActivateMachine")]
+public class SpecialItemPatch
+{
+    static void Prefix(ref ManufacturingMachineInteractive __instance)
+    {
+        // Pretty!
+        ref var self = ref __instance;
+
+        // Only works on items
+        if (self._type != Gear.Type.Item)
+        {
+            return;
+        }
+
+        // A list of all boss and adventurer items
+        // Want: a better generic solution
+        var specialItems = new string[]{
+            // Boss Items
+            "ElderEntsGratitude",
+            "UnknownSeed",
+            "GoldenManeRapier",
+            "ProofOfFellowship",
+            "ContaminatedCore",
+            "ChimerasFang",
+            "ArchbishopsBible",
+
+            // Adventurer Items
+            "TheChosenClericsBible",
+            "TheChosenHerosCirclet",
+            "TheChosenHuntersArrow",
+            "TheChosenMagesBadge",
+            "TheChosenThiefsTwinSwords",
+            "TheChosenWarriorsArmor",
+        };
+
+        // Add by order, so they're always grouped
+        // 1. Special Items
+        self._gearList.AddRange(GearResource.instance.items.Where(
+            (ItemReference item) => specialItems.Contains(item.name))
+        );
+
+        // 2. Sin Items
+        self._gearList.AddRange(GearResource.instance.items.Where(
+            (ItemReference item) => (item.prefabKeyword1 == Inscription.Key.Sin || item.prefabKeyword2 == Inscription.Key.Sin)
+        ));
+
+        // 3. Omen Items
+        self._gearList.AddRange(GearResource.instance.items.Where(
+            (ItemReference item) => (item.gearTag.HasFlag(Gear.Tag.Omen) && !item.gearTag.HasFlag(Gear.Tag.UpgradedOmen))
+        ));
+    }
+}
