dbe8b7a52571456ecc00333574556d86f97ae0bf
MrBacanudo <73220344+MrBacanudo@users.noreply.github.com>
2023-03-26 22:01:52 +0200

    Fix Balrog bugs:
    1. Allow exiting the game and returning without issues
    2. Allow equipping the Balrog quintessence after using the mod

--- Balrog/BalrogPatch.cs
+++ Balrog/BalrogPatch.cs
@@ -3,10 +3,15 @@ using Characters.Gear.Weapons;
 using static Characters.Damage;
 using UnityEngine;
 using Characters.Gear.Weapons.Gauges;
+using Characters.Player;
+using Singletons;
+using Characters;
+using Services;
+using Characters.Operations.Summon;
 
 namespace Balrog;
 
-[HarmonyPatch(typeof(Weapon), "OnLoot")]
+[HarmonyPatch(typeof(Weapon), "OnEquipped")]
 public class BalrogPatch
 {
     private static void SetTypeInChildren(GameObject self, MotionType type)
@@ -37,11 +42,31 @@ public class BalrogPatch
     static void Prefix(ref Weapon __instance)
     {
         ref var self = ref __instance;
+
+        // Only work with Balrog, of course
         if (self.name != "Balrog_Head")
         {
             return;
         }
 
+        // Allow the Balorg quintessence to deal quintessence damage
+        Character player = Singleton<Service>.Instance.levelManager.player;
+        if (player.playerComponents.inventory.weapon.polymorphWeapon == self)
+        {
+            return;
+        }
+
+        // Prevent redoing this multiple times by tagging the object with a 
+        const string dummyName = "Balrog_Mod_Tag";
+        if (self.gameObject.transform.Find(dummyName) != null)
+        {
+            return;
+        }
+
+        GameObject tag = new(dummyName);
+        tag.transform.parent = self.gameObject.transform;
+
+        // Make most damage sources deal non-quintessence damage
         SetType(self.gameObject, "Equipped/Dash_Tackle", MotionType.Dash);
         SetType(self.gameObject, "Equipped/PowerbombJumpAttack", MotionType.Basic);
         SetType(self.gameObject, "Equipped/JumpAttack", MotionType.Basic);
@@ -49,31 +74,57 @@ public class BalrogPatch
         SetType(self.gameObject, "Equipped/ComboAttack(3)", MotionType.Basic);
         SetType(self.gameObject, "Equipped/ComboAttack(4) (1)", MotionType.Basic);
 
-        // TODO: find a way that doesn't ruin using Balrog Quintessence after using this once
-        // A possibility is instantiating all pool objects, then using the referenced clones
-        // Just reset the game for now!
-        foreach (var runner in self.gameObject.transform.Find("Equipped/ComboAttack(3)").GetComponentsInChildren<Characters.Operations.Summon.SummonOperationRunner>())
+        // Make the spawned effects deal skill and basic attack damage
+        foreach (var runner in self.gameObject.transform.Find("Equipped/ComboAttack(3)").GetComponentsInChildren<SummonOperationRunner>())
         {
-            var obj = runner._operationRunner._poolObject.gameObject;
+            var poolObj = runner._operationRunner._poolObject;
+            var obj = poolObj.gameObject;
             var name = obj.name;
+
+            if (name != "Balrog_BasicAttack" && name != "Balrog_lava")
+            {
+                // Prevent creating a new unnecessary object by duplicating conditionals, because why not?
+                continue;
+            }
+
+            // Clone object, so the quintessence isn't changed
+            var clonedPoolObj = UnityEngine.Object.Instantiate<PoolObject>(poolObj);
+            UnityEngine.Object.DontDestroyOnLoad(clonedPoolObj);
+            poolObj._keepOriginal = true;
+            var clonedObj = clonedPoolObj.gameObject;
+            runner._operationRunner = clonedObj.GetComponent<OperationRunner>();
+            runner._operationRunner._poolObject = clonedPoolObj;
+
             if (name == "Balrog_BasicAttack")
             {
-                SetTypeInChildren(obj, MotionType.Basic);
+                SetTypeInChildren(clonedObj, MotionType.Basic);
             }
             else if (name == "Balrog_lava")
             {
                 // TODO: figure out the actual damage source of lava. This does nothing.
-                SetTypeInChildren(obj, MotionType.Skill);
+                SetTypeInChildren(clonedObj, MotionType.Skill);
             }
+
+            clonedObj.SetActive(false);
         }
 
-        foreach (var runner in self.gameObject.transform.Find("Equipped/Skill_Catastrophe").GetComponentsInChildren<Characters.Operations.Summon.SummonOperationRunnersOnGround>())
+        foreach (var runner in self.gameObject.transform.Find("Equipped/Skill_Catastrophe").GetComponentsInChildren<SummonOperationRunnersOnGround>())
         {
-            var obj = runner._operationRunner._poolObject.gameObject;
+            var poolObj = runner._operationRunner._poolObject;
+            var obj = poolObj.gameObject;
             var name = obj.name;
+
             if (name == "Balrog_Skill")
             {
-                SetTypeInChildren(obj, MotionType.Skill);
+                var clonedPoolObj = UnityEngine.Object.Instantiate<PoolObject>(poolObj);
+                UnityEngine.Object.DontDestroyOnLoad(clonedPoolObj);
+                poolObj._keepOriginal = true;
+                var clonedObj = clonedPoolObj.gameObject;
+                runner._operationRunner = clonedObj.GetComponent<OperationRunner>();
+                runner._operationRunner._poolObject = clonedPoolObj;
+
+                SetTypeInChildren(clonedObj, MotionType.Skill);
+                clonedObj.SetActive(false);
             }
         }
 
